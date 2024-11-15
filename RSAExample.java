54cbc774dc217b5f2297d0873d274c08b1e218e8
dadobaxxx <simondodavide@gmail.com>
2024-11-15 12:36:00 +0000

    whoutmain

---
+++ RSAExample.java
@@ -0,0 +1,26 @@
+import java.math.BigInteger;
+import java.security.SecureRandom;
+import java.util.logging.Logger;
+
+public class RSAExample {
+    private static final Logger logger = Logger.getLogger(RSAExample.class.getName());
+    private BigInteger n, d, e, p, q;
+
+    private RSAExample (int bitlen){
+        SecureRandom random = new SecureRandom();
+        p= new BigInteger(bitlen, 100, random);
+        q= new BigInteger(bitlen, 100, random);
+
+        n= p.multiply(q);
+        BigInteger v = (p.subtract(BigInteger.ONE)).multiply(p.subtract(BigInteger.ONE));
+
+        do{
+            e= BigInteger.probablePrime(bitlen, random);
+        } while (e.gcd(v).equals(BigInteger.ONE));
+
+        d= e.modInverse(v);
+
+    }
+
+    
+}
